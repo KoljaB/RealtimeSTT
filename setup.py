7b9d315bd82eb5cd5d54553827902d76fc8b822a
KoljaB <kolja.beigel@web.de>
2024-08-07 14:42:55 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Read requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="RealtimeSTT",
+    version="0.2.1",
+    author="Kolja Beigel",
+    author_email="kolja.beigel@web.de",
+    description="A fast Voice Activity Detection and Transcription System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KoljaB/RealTimeSTT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+    keywords="real-time, audio, transcription, speech-to-text, voice-activity-detection, VAD, real-time-transcription, ambient-noise-detection, microphone-input, faster_whisper, speech-recognition, voice-assistants, audio-processing, buffered-transcription, pyaudio, ambient-noise-level, voice-deactivity"
+)
